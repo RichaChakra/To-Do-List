3b6a78687e570b1bfbd74d19bbc5585c7abf4761
Richa Chakraborty <151943279+RichaChakra@users.noreply.github.com>
2024-07-26 19:15:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import ToDoList from './ToDoList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
